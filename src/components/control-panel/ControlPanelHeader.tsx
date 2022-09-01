84aaa67d0be536a84f542719e0911ef5733d98e0
Tiago Santos <tfsantosbr@gmail.com>
2022-09-01 07:53:31 -0300

    update componentes

--- src/components/control-panel/ControlPanelHeader.tsx
+++ src/components/control-panel/ControlPanelHeader.tsx
@@ -1,3 +1,14 @@
+import { NavLink } from "react-router-dom";
+
 export function ControlPanelHeader() {
-    return <h1>Control Panel Header</h1>
+  return (
+    <>
+      <h1>Control Panel Header (<NavLink to="/">Go To Blog</NavLink>)</h1>
+      <nav>
+        <NavLink to="/control-panel">Dashboard</NavLink> | 
+        <NavLink to="/control-panel/companies">Companies</NavLink> | 
+        <NavLink to="/control-panel/users">Users</NavLink>
+      </nav>
+    </>
+  );
 }
--- src/pages/blog/Home.tsx
+++ src/pages/blog/Home.tsx
@@ -1,3 +1,10 @@
+import { NavLink } from "react-router-dom";
+
 export function Home() {
-    return <h1>Blog Home Page</h1>
+  return (
+    <>
+      <h1>Blog Home Page</h1>
+      <NavLink to="/control-panel">Go To Control Panel</NavLink>
+    </>
+  );
 }
